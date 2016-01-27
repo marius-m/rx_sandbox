43c7344d59a814ab49f9e3caf481e6794334f7e7
Marius Merkevicius <marius.m@ito.lt>
2016-01-27 19:23:57 +0200

    Adding lambdas.

--- app/src/main/java/lt/markmerkk/Main.java
+++ app/src/main/java/lt/markmerkk/Main.java
@@ -13,12 +13,7 @@ public class Main {
 
   public static void main(String[] args) {
     Observable.just("observable")
-        .map(new Func1<String, String>() {
-          @Override
-          public String call(String s) {
-            return s+s;
-          }
-        })
+        .map(s -> s+s)
         .subscribe(s -> System.out.println("subscriber = "+s));
   }
 
