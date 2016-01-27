9ba6683683f527fbfd4ddc278cc25f35355a8271
Marius Merkevicius <marius.m@ito.lt>
2016-01-27 19:23:17 +0200

    Modifying output of observable.

--- app/src/main/java/lt/markmerkk/Main.java
+++ app/src/main/java/lt/markmerkk/Main.java
@@ -2,6 +2,7 @@ package lt.markmerkk;
 
 import rx.Observable;
 import rx.functions.Action1;
+import rx.functions.Func1;
 
 /**
  * Created by mariusmerkevicius on 1/27/16.
@@ -11,15 +12,14 @@ public class Main {
 
 
   public static void main(String[] args) {
-//    Observable.just("Something").subscribe(new Action1<String>() {
-//      @Override
-//      public void call(String s) {
-//        System.out.println("Somehing +" + s);
-//      }
-//    });
-    Observable.just("Something").subscribe(s -> {
-      System.out.println("Something +"+s);
-    });
+    Observable.just("observable")
+        .map(new Func1<String, String>() {
+          @Override
+          public String call(String s) {
+            return s+s;
+          }
+        })
+        .subscribe(s -> System.out.println("subscriber = "+s));
   }
 
 }
