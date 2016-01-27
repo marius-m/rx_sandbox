a04aded8962453deebc72f3dc08c248a584ad5eb
Marius Merkevicius <marius.m@ito.lt>
2016-01-27 19:15:19 +0200

    Subscriber with lots of boilerplate.

--- app/src/main/java/lt/markmerkk/Main.java
+++ app/src/main/java/lt/markmerkk/Main.java
@@ -9,9 +9,32 @@ import rx.functions.Action1;
  */
 public class Main {
 
+
+
   public static void main(String[] args) {
-    
+    myObservable.subscribe(mySubscriber);
   }
+
+  static Observable<String> myObservable = Observable.create(
+      new Observable.OnSubscribe<String>() {
+        @Override
+        public void call(Subscriber<? super String> sub) {
+          sub.onNext("Hello, world!");
+          sub.onCompleted();
+        }
+      }
+  );
+
+  static Subscriber<String> mySubscriber = new Subscriber<String>() {
+    @Override
+    public void onNext(String s) { System.out.println(s); }
+
+    @Override
+    public void onCompleted() { }
+
+    @Override
+    public void onError(Throwable e) { }
+  };
 
 }
 
