19824aa3f256175f7063e1c847c1d37d75dd513c
Marius Merkevicius <marius.m@ito.lt>
2016-01-27 20:33:52 +0200

    Outputting a list of inputs

--- app/src/main/java/lt/markmerkk/Main.java
+++ app/src/main/java/lt/markmerkk/Main.java
@@ -1,6 +1,9 @@
 package lt.markmerkk;
 
+import java.util.ArrayList;
+import java.util.List;
 import rx.Observable;
+import rx.Subscriber;
 import rx.functions.Action1;
 import rx.functions.Func1;
 
@@ -9,12 +12,27 @@ import rx.functions.Func1;
  */
 public class Main {
 
-
+  // Returns a List of website URLs based on a text search
+  static Observable<List<String>> query(String text) {
+    return Observable.create(new Observable.OnSubscribe<List<String>>() {
+      @Override
+      public void call(Subscriber<? super List<String>> subscriber) {
+        subscriber.onNext(new ArrayList<String>(){{
+          add("one");
+          add("two");
+          add("three");
+        }});
+        subscriber.onCompleted();
+      }
+    });
+  }
 
   public static void main(String[] args) {
-    Observable.just("123")
-        .map(s -> Integer.parseInt(s)+1)
-        .subscribe(i -> System.out.println("subscriber = "+i));
+    query("Hello, world!")
+        .subscribe(urls -> {
+          for (String url : urls)
+            System.out.println(url);
+        });
   }
 
 }
