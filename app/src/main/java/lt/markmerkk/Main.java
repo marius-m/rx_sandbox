1251459b3c09cf7eed21ece17eb30b2ee43961fc
Marius Merkevicius <marius.m@ito.lt>
2016-01-27 19:07:41 +0200

    Initial commit, take two.

---
+++ app/src/main/java/lt/markmerkk/Main.java
@@ -0,0 +1,17 @@
+package lt.markmerkk;
+
+import rx.Observable;
+import rx.Subscriber;
+import rx.functions.Action1;
+
+/**
+ * Created by mariusmerkevicius on 1/27/16.
+ */
+public class Main {
+
+  public static void main(String[] args) {
+    
+  }
+
+}
+
